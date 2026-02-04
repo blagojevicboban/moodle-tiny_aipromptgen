e6e09d62462707c00d1974f5ec1fae5fa6e361da
Boban Blagojevic <boban.blagojevic@tsp.edu.rs>
2026-02-04 01:54:06 +0100

    chore: Update the plugin version and release numbers.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_aipromptgen';
-$plugin->version = 2026020101;
-$plugin->release = '1.1';
+$plugin->version = 2026020402;
+$plugin->release = '1.2';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2022041900; // Moodle 4.0+.
